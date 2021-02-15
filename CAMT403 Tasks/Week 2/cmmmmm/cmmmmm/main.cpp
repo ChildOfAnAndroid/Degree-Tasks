0d473321ca56d49bdb79cc5cb03396d730681f99
Charis Cat <65819058+CharisCat@users.noreply.github.com>
2021-02-15 16:36:52 +0000

    not sure

---
+++ CAMT403 Tasks/Week 2/cmmmmm/cmmmmm/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  cmmmmm
+//
+//  Created by Charis Cat on 14/02/2021.
+//
+
+//bitch i love frogs
+
+#include <iostream>
+
+int main() {
+
+std::cout << "  CCC  M   M\nl";
+std::cout << "C   C MM MM\nl";
+std::cout << "C     MM MM\nl";
+std::cout << "C     M M M\nl";
+std::cout << "C     M   M\nl";
+std::cout << "C   C M   M\nl";
+std::cout << " CCC  M   M\nl";
+
+}
