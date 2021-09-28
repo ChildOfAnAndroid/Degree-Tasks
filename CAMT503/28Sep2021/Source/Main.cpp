e6f069ca600e8dc1e3af2eabcea9f8087e461056
Charis Cat <65819058+CharisCat@users.noreply.github.com>
2021-09-28 16:35:35 +0100

    end of first class
    might want to look at how to access all elements of the array for this again

--- CAMT503/28Sep2021/Source/Main.cpp
+++ CAMT503/28Sep2021/Source/Main.cpp
@@ -11,6 +11,8 @@
 //==============================================================================
 int main (int argc, char* argv[]){
 
+//CHARACTERS IN A NAME
+    
     for(int i = 0; i < 100; i++){
         
 //input name
@@ -31,6 +33,39 @@ int main (int argc, char* argv[]){
 
 //stop loop when name length 1
         if (name.length() < 2){
+            break;
+        };
+        
+    } //for end
+    
+    
+//IS IT IN C MAJ SCALE??
+    
+    for(int i = 0; i < 100; i++){
+        
+//input note
+        std::string note;
+        std::cout << "Enter Note: ";
+        std::cin >> note;
+    
+//is the note in CMaj scale?
+        std::string CMaj[] = {"A", "B", "C", "D", "E", "F", "G"};
+    
+//output whether the note is in CMaj
+        std::cout << "The note ";
+        std::cout << note;
+            //if yes then 'is' if no then 'is not'
+        if(note == CMaj[0]){
+                std::cout << "is";
+            }
+            else{
+                std::cout << "is not";
+            }
+        
+        std::cout << " in CMaj" << std::endl;
+
+//stop loop if note name is greater than 2 characters
+        if (note.length() > 2){
             break;
         };
         
