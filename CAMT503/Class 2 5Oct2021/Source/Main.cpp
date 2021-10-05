a3981c9aa3baa58a1f2d2b9b87328d6e8fe212c6
Charis Cat <65819058+CharisCat@users.noreply.github.com>
2021-10-05 15:58:41 +0100

    end of class 2

--- CAMT503/Class 2 5Oct2021/Source/Main.cpp
+++ CAMT503/Class 2 5Oct2021/Source/Main.cpp
@@ -7,13 +7,56 @@
 */
 
 #include <JuceHeader.h>
+#include <algorithm>
+
+int transpose_key(int note, int diff)
+{
+    return note + diff;
+}
 
 //==============================================================================
 int main (int argc, char* argv[])
 {
 
     // ..your code goes here!
-
+    std::vector<int> c_major_notes;
+    int first_octave[7] = {24, 26, 28, 29, 31, 33, 35};
+    
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            c_major_notes.push_back(first_octave[j] + i * 12);
+        }
+    }
+    
+    for (auto i : c_major_notes)
+        std::cout << i << " ";
+    
+    int note;
+    
+    std::cin >> note;
+    
+    auto it = std::find(c_major_notes.begin(), c_major_notes.end(), note);
+    
+    if (it != c_major_notes.end())
+    {
+        auto pos = it - c_major_notes.begin();
+        std::cout << c_major_notes[pos + 1];
+    }
+    
+    
+    //int note, diff;
+    
+    //std::cout << "Enter MIDI Note: ";
+    //std::cin >> note;
+    
+    //std::cout << "Transpose by: ";
+    //std::cin >> diff;
+    
+    //std::cout << transpose_key(note, diff);
+    //std::cout << std::endl;
+    
 
     return 0;
 }
