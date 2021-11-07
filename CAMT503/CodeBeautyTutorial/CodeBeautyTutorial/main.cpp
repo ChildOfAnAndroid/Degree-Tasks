4dcc7b92e658cd42a5ec06dd01880fff5f4b3d45
Charis Cat <65819058+CharisCat@users.noreply.github.com>
2021-11-07 21:15:54 +0000

    fin

--- CAMT503/CodeBeautyTutorial/CodeBeautyTutorial/main.cpp
+++ CAMT503/CodeBeautyTutorial/CodeBeautyTutorial/main.cpp
@@ -15,9 +15,11 @@ class AbstractEmployee {
 
 class Employee:AbstractEmployee {
 private: //properties are hidden/encapsulated within the class
-    string Name;
     string Company;
     int Age;
+    
+protected: //accessible from derived classes
+    string Name;
     
 public:
     //Setter
@@ -66,9 +68,46 @@ public:
         else
             std::cout << Name << ", sorry no promotion for you!" << std::endl;
     }
+    
+    virtual void Work() { //Virtual functions search for identical functions in their child classes and then implement the furthest derived form of the function
+        std::cout << Name << " is checking email, checking task backlog, performing those tasks..." << std::endl;
+    }
 };
 
+class Developer:public Employee { //colon then putting the parent class after means developer is now a child class of employee
+    //developer now has all of the properties that employee has
+    //can use public before parent class to inherit the properties publically
+    
+public:
+    string FavProgrammingLanguage;
+    Developer(string name, string company, int age, string favProgrammingLanguage)
+        :Employee(name, company, age)
+    {
+        FavProgrammingLanguage = favProgrammingLanguage;
+    }
+    void FixBug() {
+        std::cout << getName() << " fixed a bug using " << FavProgrammingLanguage << std::endl;
+    }
+    void Work() {
+        std::cout << Name << " is writing a " << FavProgrammingLanguage << " program" << std::endl;
+    }
+};
 
+class Teacher:public Employee {
+public:
+    string Subject;
+    void PrepareLesson() {
+        std::cout << Name << " is preparing a " << Subject <<  " lesson" << std::endl;
+    }
+    Teacher(string name, string company, int age, string subject)
+        :Employee(name, company, age)
+    {
+        Subject = subject;
+    }
+    void Work() {
+        std::cout << Name << " is teaching a " << Subject << " class" << std::endl;
+    }
+};
 
 int main() {
 
@@ -84,8 +123,36 @@ int main() {
     employee1.AskForPromotion();
     employee2.AskForPromotion();
     
+    Developer d = Developer("Charis", "Tindie", 26, "C++");
+    
+    d.FixBug();
+    
+    d.Work();
+    
+    d.AskForPromotion();
+    
+    Teacher t = Teacher ("Satvik", "Plymouth University", 28, "Programming");
+    
+    t.PrepareLesson();
+    
+    t.Work();
+    
+    t.AskForPromotion();
+    
+    // The most common use of polymorphism is when a parent class reference is used to refer to a child class object.
+    
+    Employee* e1 = &d; //pointer of parent class holds reference of child class object
+    Employee* e2 = &t;
+    
+    e1->Work(); // -> is used when you want to access members using a pointer, used instead of . symbol when pointers are involved
+    e2->Work();
+    
 }
 
 //Encapsulation - bundling data & methods of changing that data into one class. Other classes cannot directly change the data.
 
 //Abstraction - hiding complex things behind a procedure that simplifies them
+
+//Inheritance - parent vs child class. parent class has members, child class can inherit members from parent class.
+
+//Polymorphism - The ability of an object or method to have many forms. The most common use of polymorphism is when a parent class reference is used to refer to a child class object.
