c77733727bc04ce1ae8de30b5ccb61417bdb5325
Charis Cat <65819058+CharisCat@users.noreply.github.com>
2021-12-03 09:45:07 +0000

    start of class

---
+++ CAMT503/SoundSynthBuiltInClass/Source/PluginProcessor.cpp
@@ -0,0 +1,322 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+SoundSynthAudioProcessor::SoundSynthAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+    // Add voices and sound to the synth object.
+    for (auto i = 0; i < 5; i++)
+    {
+        synth.addVoice(new SineWaveVoice());
+    }
+    
+    synth.addSound(new SineWaveSound());
+}
+
+SoundSynthAudioProcessor::~SoundSynthAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String SoundSynthAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool SoundSynthAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SoundSynthAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SoundSynthAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double SoundSynthAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int SoundSynthAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int SoundSynthAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void SoundSynthAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String SoundSynthAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void SoundSynthAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void SoundSynthAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+    synth.setCurrentPlaybackSampleRate(sampleRate);
+}
+
+void SoundSynthAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool SoundSynthAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void SoundSynthAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer (channel);
+
+        // ..do something to the data...
+    }
+    
+    auto audioBusBuffer = getBusBuffer(buffer, false, 0);
+    
+    synth.renderNextBlock(audioBusBuffer, midiMessages, 0, audioBusBuffer.getNumSamples());
+    
+}
+
+//==============================================================================
+bool SoundSynthAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* SoundSynthAudioProcessor::createEditor()
+{
+    return new SoundSynthAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void SoundSynthAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void SoundSynthAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new SoundSynthAudioProcessor();
+}
+
+
+bool SineWaveSound::appliesToNote (int midiNoteNumber)
+{
+    return true;
+}
+bool SineWaveSound::appliesToChannel (int midiChannel)
+{
+    return true;
+}
+
+SineWaveVoice::SineWaveVoice() : currentAngle(0.0), angleDelta(0.0), level(0.0), tailOff(0.0)
+{
+    
+}
+
+
+bool SineWaveVoice::canPlaySound (juce::SynthesiserSound* sound)
+{
+    return dynamic_cast<SineWaveSound*>(sound) != nullptr;
+}
+
+void SineWaveVoice::startNote (int midiNoteNumber, float velocity, juce::SynthesiserSound *sound, int currentPitchWheelPosition)
+
+{
+    currentAngle = 0.0;
+    level = velocity * 0.25;
+    tailOff = 0.0;
+    
+    auto cyclesPerSecond = juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber);
+    auto cyclesPerSample = cyclesPerSecond / getSampleRate();
+    
+    angleDelta = cyclesPerSample * 2.0 * juce::MathConstants<double>::pi;
+}
+
+void SineWaveVoice::stopNote (float velocity, bool allowTailOff)
+{
+    if (allowTailOff)
+    {
+        if (tailOff == 0.0)
+        {
+            tailOff = 1.0;
+        }
+    }
+    
+    else
+    {
+        clearCurrentNote();
+        angleDelta = 0.0;
+    }
+}
+
+void SineWaveVoice::pitchWheelMoved (int newPitchWheelValue)
+{
+    
+    int maxValue = 16383;
+    int minValue = 0;
+    int meanValue = 8192;
+    
+    juce::Logger::writeToLog("Pitch Wheel Value: " + std::to_string(newPitchWheelValue));
+    
+    // HIGHEST VALUE: 16383
+    // LOWEST VALUE: 0
+    // MID VALUE: 8192
+    
+}
+
+void SineWaveVoice::controllerMoved (int controllerNumber, int newControllerValue)
+{
+    
+}
+
+void SineWaveVoice::renderNextBlock (juce::AudioBuffer< float > &outputBuffer, int startSample, int numSamples)
+{
+    if (angleDelta != 0.0)
+    {
+        if (tailOff > 0.0)
+        {
+            while (--numSamples >= 0)
+            {
+                auto currentSample = (float)((std::sin(currentAngle) + std::sin(1.5*currentAngle)) * tailOff * level);
+                
+                for (auto i = outputBuffer.getNumChannels();--i >=0;)
+                {
+                    outputBuffer.addSample(i, startSample, currentSample);
+                }
+                
+                currentAngle += angleDelta;
+                ++startSample;
+                
+                tailOff = tailOff * 0.99;
+                
+                if (tailOff <= 0.005)
+                {
+                    clearCurrentNote();
+                    angleDelta = 0.0;
+                    break;
+                }
+            }
+        }
+        
+        else
+        {
+            while (--numSamples >= 0)
+            {
+                auto currentSample = (float)((std::sin(currentAngle) + std::sin(1.5*currentAngle)) * level);
+                
+                for (auto i = outputBuffer.getNumChannels();--i >=0;)
+                {
+                    outputBuffer.addSample(i, startSample, currentSample);
+                }
+                
+                currentAngle += angleDelta;
+                ++startSample;
+            }
+        }
+    }
+}
---
+++ CAMT503/SoundSynthBuiltInClass/Source/PluginProcessor.h
@@ -0,0 +1,94 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/**
+*/
+class SoundSynthAudioProcessor  : public juce::AudioProcessor
+{
+public:
+    //==============================================================================
+    SoundSynthAudioProcessor();
+    ~SoundSynthAudioProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+private:
+    //==============================================================================
+    
+    juce::Synthesiser synth;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SoundSynthAudioProcessor)
+};
+
+
+// Step 1: Create a new class called SineWaveSound
+struct SineWaveSound : public juce::SynthesiserSound
+{
+    SineWaveSound() { };
+    
+    bool appliesToNote (int midiNoteNumber) override;
+    bool appliesToChannel (int midiChannel) override;
+};
+// Go to PluginProcessor.cpp to define the functions
+
+
+// Step 3: Declare the SineWaveVoice class
+struct SineWaveVoice : public juce::SynthesiserVoice
+{
+    SineWaveVoice();
+    
+    bool canPlaySound (juce::SynthesiserSound* sound) override;
+    void startNote (int midiNoteNumber, float velocity, juce::SynthesiserSound *sound, int currentPitchWheelPosition) override;
+    void stopNote (float velocity, bool allowTailOff) override;
+    
+    void pitchWheelMoved (int newPitchWheelValue) override;
+    void controllerMoved (int controllerNumber, int newControllerValue) override;
+        
+    void renderNextBlock (juce::AudioBuffer< float > &outputBuffer, int startSample, int numSamples) override;
+    
+    
+private:
+    double currentAngle, angleDelta, level, tailOff;
+};
+// Go to PluginProcessor.cpp to define the functions.
